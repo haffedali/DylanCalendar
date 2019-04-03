aa1ca11c9a6a9b9e5815f58de6ad0cd8b64c298d
Hoff <haffedmcnair@gmail.com>
2019-04-02 18:31:14 -0700

    TODO gather and display data from db

--- client/src/components/Day/Day.js
+++ client/src/components/Day/Day.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import CreateEvent from "../CreateEvent"
+import "bootstrap/dist/css/bootstrap.min.css";
 import './Day.css';
 
 export default class Day extends Component {
@@ -8,14 +9,25 @@ export default class Day extends Component {
     // 1. Is constructed with events and event data passed as props
     // 2. Create event button opens modal that displays the CreateEvent component
     // 3. Date is passed as prop to be displayed
+    // 4. Need to pass down function for create event, instead make it simply return the date of the day it clicked to parent component
+    // and the parent component (Calendar) renders the CreateEvent component. It will look like the + button does it, but we sneaky sneaky 
     constructor(props){
         super(props);
 
+        this.onCreateEventClick = this.onCreateEventClick.bind(this)
         this.state = {
-        
+            isModalOpen: false,
         }
     }
 
+    onCreateEventClick(e){
+        // e.preventDefault();
+
+        this.setState({
+            isModalOpen: true,
+        })
+
+    }
 
     render(){
         return(
@@ -23,7 +35,7 @@ export default class Day extends Component {
                 <div className='box'>
                     <div className='heading'>
                         <div className='date'>date</div>
-                        <div className='button'>+</div>
+                        <button onClick={this.onCreateEventClick}>+</button>
                     </div>
                     {/* will need to populate this with data from props */}
                     <div className='body'>
